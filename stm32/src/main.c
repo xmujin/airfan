c687d1bb5eb9b36e01f0f1c29e2d6216a605524a
xmujin <806181859@qq.com>
2024-05-30 17:38:18 +0800

    OLED 的显示

--- stm32/src/main.c
+++ stm32/src/main.c
@@ -10,34 +10,31 @@
 */
 
 #include "stm32f10x.h"
+#include "OLED.h"
+#include "delay.h"
+
 
 #define LED_PERIPH RCC_APB2Periph_GPIOA
 #define LED_PORT GPIOA
 #define LED_PIN GPIO_Pin_1
 
-void delay(int x)
-{
-    for (int i = 0; i < x; i++)
-    {
-        for (int j = 0; j < 1000; j++)
-            __NOP();
-    }
-}
+
 
 int main()
 {
-    GPIO_InitTypeDef initStruct; 
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE); // 配置时钟GPIOA
-	initStruct.GPIO_Pin = GPIO_Pin_0 ;
-	initStruct.GPIO_Mode = GPIO_Mode_Out_PP;
-	initStruct.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_Init(GPIOA, &initStruct);
-	GPIO_WriteBit(GPIOA, GPIO_Pin_0, Bit_RESET);
+    // GPIO_InitTypeDef initStruct; 
+	// RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE); // 配置时钟GPIOA
+	// initStruct.GPIO_Pin = GPIO_Pin_0 ;
+	// initStruct.GPIO_Mode = GPIO_Mode_Out_PP;
+	// initStruct.GPIO_Speed = GPIO_Speed_50MHz;
+	// GPIO_Init(GPIOA, &initStruct);
+    // delay_init();
+    // delay_ms(1000);
+    OLED_Init();
+    OLED_ShowString(1, 1, "Hello World!");
+                         
     while (1)
     {
-        // GPIO_WriteBit(GPIOA, GPIO_Pin_0, Bit_SET);
-        // delay(2000);
-        // GPIO_WriteBit(GPIOA, GPIO_Pin_0, Bit_RESET);
-        // delay(2000);
+
     }
 }
